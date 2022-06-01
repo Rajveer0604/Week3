a1e25ce74622f0b1e63728dfc60357582b5365ab
Rajveer0604 <jatra@sheridanc.on.ca>
2022-06-01 09:31:12 -0400

    new repo for student code

---
+++ src/java/rajveer/JavaRajveer.java
@@ -0,0 +1,22 @@
+/*
+ * RajdeepKaur Jat
+ * Student ID: 991625547
+ * SYST10199 - Web Programming
+ */
+package java.rajveer;
+
+/**
+ *
+ * @author 16479
+ */
+public class JavaRajveer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/java/rajveer/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * RajdeepKaur Jat
+ * Student ID: 991625547
+ * SYST10199 - Web Programming
+ */
+package java.rajveer;
+
+/**
+ *
+ * @author 16479
+ */
+public class NewClass {
+    public static void main(String[]args){
+    System.out.print("hello");
+    }
+}
